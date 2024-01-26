35d969c37b7fba8f84f05d7e8eefcce391a70ebb
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2024-01-27 00:11:11 +0100

    Add files via upload

---
+++ terre_ok.py
@@ -0,0 +1,6 @@
+def retourne_liste(input_chaine , input_liste) :
+    return f"{input_chaine} {input_liste}"
+
+input_chaine = "J'ai terminé l'Epreuve de la terre et c'etait"
+input_liste = ["Long"]
+print(retourne_liste(input_chaine , input_liste))
