5295c000c4c95fbe4c1fb2eef2227bdcc4da4d40
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2024-01-26 23:57:48 +0100

    Add files via upload

---
+++ Affiche_alphabet.py
@@ -0,0 +1,26 @@
+
+import sys
+
+affiche_alphabet_minuscule  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n"
+affiche_alphabet_majuscule  = "vous devais utiliser un autre fichier"
+message0 = "inconnue"
+affiche_alphabet1 = affiche_alphabet_minuscule.lower()
+
+
+    
+def affiche_alphabet0 ():
+    print(affiche_alphabet1)
+     
+while affiche_alphabet_minuscule == affiche_alphabet_majuscule :
+        print(message0.lower())
+else :
+         affiche_alphabet0 ()
+
+
+    
+
+
+
+
+#"abcdefghijklmnopqrstuvwxyz\n"
+#"ABCDEFGHIJKLMNOPQRSTUVWXYZ\n"
